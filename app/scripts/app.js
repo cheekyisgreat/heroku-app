efdfaf09f68bf101331e268b614f666a47c896e9
Dennis Dominguez <cheekyisgreat@gmail.com>
2015-02-22 10:38:59 -0500

    Add angular seed and dependencies without any management

---
+++ app/scripts/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name appApp
+ * @description
+ * # appApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('appApp', [
+    'ngAnimate',
+    'ngRoute',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
